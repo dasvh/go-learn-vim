70ff899691342aba143355781d9e8faaa6a9817f
dario <dasvh@protonmail.com>
2024-12-21 18:22:46 +0100

    feat: manager.go operate on tea.Model instead of ui.View

--- internal/game/state/manager.go
+++ internal/game/state/manager.go
@@ -2,28 +2,25 @@ package state
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
-	ui "github.com/dasvh/go-learn-vim/internal/components/view"
 )
 
-// ViewManager handles the management and switching of game views/screens
+// ViewManager handles the management and switching of game models
 type ViewManager struct {
 	activeView GameScreen
-	views      map[GameScreen]ui.View
+	views      map[GameScreen]tea.Model
 }
 
 // NewManager creates and returns a new ViewManager instance with MainMenuScreen as the default active view
 func NewManager() *ViewManager {
 	return &ViewManager{
 		activeView: MainMenuScreen,
-		views:      make(map[GameScreen]ui.View),
+		views:      make(map[GameScreen]tea.Model),
 	}
 }
 
 // Register adds a new view to the ViewManager
 func (vm *ViewManager) Register(view GameScreen, model tea.Model) {
-	if v, ok := model.(ui.View); ok {
-		vm.views[view] = v
-	}
+	vm.views[view] = model
 }
 
 // CurrentView returns the currently active view model from the ViewManager
@@ -47,9 +44,5 @@ func (vm *ViewManager) ActiveView() GameScreen {
 
 // Views returns a map containing all registered views
 func (vm *ViewManager) Views() map[GameScreen]tea.Model {
-	views := make(map[GameScreen]tea.Model)
-	for k, v := range vm.views {
-		views[k] = v
-	}
-	return views
+	return vm.views
 }
