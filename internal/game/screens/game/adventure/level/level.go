53c4d925cc8134cfc6cb6489972e687f63d5e895
dario <dasvh@protonmail.com>
2024-12-27 15:34:30 +0100

    wip: use a target type to determine target rune

--- internal/game/screens/game/adventure/level/level.go
+++ internal/game/screens/game/adventure/level/level.go
@@ -1,13 +1,17 @@
 package level
 
+import (
+	"fmt"
+	"github.com/dasvh/go-learn-vim/internal/game/screens/game/adventure/character"
+)
+
 type Level interface {
 	Init(width, height int)
 	Update(playerX, playerY int) bool
 	GetStartPosition() (int, int)
 	GetGrid() [][]rune
 	GetInstructions() string
-	GetTargetCharacter() rune
-	GetTargetReachedCharacter() rune
+	GetCharacters() *character.Characters
 	IsCompleted() bool
 }
 
@@ -16,38 +20,46 @@ type Position struct {
 	Y int
 }
 
+type target struct {
+	position Position
+	reached  bool
+}
+
 type LevelZero struct {
 	grid      [][]rune
 	current   int
 	width     int
 	height    int
-	targets   []Position
+	targets   []target
 	completed bool
 	init      bool
+	chars     *character.Characters
+}
+
+func (level0 *LevelZero) GetCharacters() *character.Characters {
+	return level0.chars
 }
 
 func NewLevelZero() Level {
-	return &LevelZero{}
-}
-
-func (level0 *LevelZero) GetTargetCharacter() rune {
-	return 'X'
-}
-
-func (level0 *LevelZero) GetTargetReachedCharacter() rune {
-	return '✓'
+	return &LevelZero{
+		chars: &character.DefaultCharacters,
+	}
 }
 
 func (level0 *LevelZero) Init(width, height int) {
 	level0.width = width
 	level0.height = height
 
+	// offset targets from the border by 20%
+	offsetX := int(float64(width) * 0.2)
+	offsetY := int(float64(height) * 0.2)
+
 	// define targets
-	level0.targets = []Position{
-		{0, 0},                  // top left
-		{width - 1, 0},          // top right
-		{0, height - 1},         // bottom left
-		{width - 1, height - 1}, // bottom right
+	level0.targets = []target{
+		{Position{offsetX, offsetY}, false},                          // top left
+		{Position{width - offsetX - 1, offsetY}, false},              // top right
+		{Position{offsetX, height - offsetY - 1}, false},             // bottom left
+		{Position{width - offsetX - 1, height - offsetY - 1}, false}, // bottom right
 	}
 
 	level0.current = 0
@@ -67,9 +79,20 @@ func (level0 *LevelZero) initializeGrid() {
 		level0.init = true
 	}
 
-	if level0.current < len(level0.targets) {
-		target := level0.targets[level0.current]
-		level0.grid[target.Y][target.X] = level0.GetTargetCharacter()
+	// set player
+	level0.grid[level0.height/2][level0.width/2] = level0.chars.Player.Cursor.Rune
+
+	// todo: figure out how to set the characters even after the game is completed
+	for i, target := range level0.targets {
+		if i == level0.current {
+			level0.grid[target.position.Y][target.position.X] = level0.chars.Target.Active.Rune
+		} else {
+			if target.reached {
+				level0.grid[target.position.Y][target.position.X] = level0.chars.Target.Reached.Rune
+			} else {
+				level0.grid[target.position.Y][target.position.X] = level0.chars.Target.Inactive.Rune
+			}
+		}
 	}
 }
 
@@ -77,8 +100,8 @@ func (level0 *LevelZero) Update(playerX, playerY int) bool {
 	target := level0.targets[level0.current]
 
 	// check if player reached target
-	if playerX == target.X && playerY == target.Y {
-		level0.grid[target.Y][target.X] = level0.GetTargetReachedCharacter()
+	if playerX == target.position.X && playerY == target.position.Y {
+		level0.targets[level0.current].reached = true
 
 		// check if this was the last target
 		if level0.current == len(level0.targets)-1 {
@@ -102,7 +125,7 @@ func (level0 *LevelZero) GetGrid() [][]rune {
 }
 
 func (level0 *LevelZero) GetInstructions() string {
-	return "Reach the X in each corner using hjkl keys"
+	return fmt.Sprintf("Target %d/%d: Reach the X using hjkl keys", level0.current+1, len(level0.targets))
 }
 
 func (level0 *LevelZero) IsCompleted() bool {
