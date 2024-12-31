fbe57291c8644942b3b50cd3324d3ed228940275
dario <dasvh@protonmail.com>
2024-12-31 17:59:15 +0100

    refactor: add text_display component for potential navigation/header items

---
+++ internal/components/text_display.go
@@ -0,0 +1,22 @@
+package components
+
+import (
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// TextDisplay displays styled text
+type TextDisplay struct {
+	Text  string
+	Style lipgloss.Style
+}
+
+// SetText sets the display text for the component
+func (dc *TextDisplay) SetText(format string, args ...interface{}) {
+	dc.Text = fmt.Sprintf(format, args...)
+}
+
+// Render generates the styled text
+func (dc *TextDisplay) Render() string {
+	return dc.Style.Render(dc.Text)
+}
